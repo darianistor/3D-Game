da7656f03c8be12420428fb00ec3109d88829c7c
darianistor <118392703+darianistor@users.noreply.github.com>
2023-03-29 22:52:12 +0300

    Files upload

---
+++ GameEngine/main.cpp
@@ -0,0 +1,412 @@
+#include "Graphics\window.h"
+#include "Camera\camera.h"
+#include "Shaders\shader.h"
+#include "Model Loading\mesh.h"
+#include "Model Loading\texture.h"
+#include "Model Loading\meshLoaderObj.h"
+#include "Model Loading\stb_image.h"
+using namespace std;
+
+
+void processKeyboardInput ();
+
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+Window window("Magic Forest", 800, 800);
+Camera camera;
+
+glm::vec3 lightColor = glm::vec3(1.0f);
+glm::vec3 lightPos = glm::vec3(-180.0f, 100.0f, -200.0f);
+
+
+
+float skyboxVertices[] = {
+	// positions          
+	-1.0f,  1.0f, -1.0f,
+	-1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+	 1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+
+	-1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f, -1.0f,
+	-1.0f,  1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
+
+	 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+
+	-1.0f, -1.0f,  1.0f,
+	-1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f, -1.0f,  1.0f,
+	-1.0f, -1.0f,  1.0f,
+
+	-1.0f,  1.0f, -1.0f,
+	 1.0f,  1.0f, -1.0f,
+	 1.0f,  1.0f,  1.0f,
+	 1.0f,  1.0f,  1.0f,
+	-1.0f,  1.0f,  1.0f,
+	-1.0f,  1.0f, -1.0f,
+
+	-1.0f, -1.0f, -1.0f,
+	-1.0f, -1.0f,  1.0f,
+	 1.0f, -1.0f, -1.0f,
+	 1.0f, -1.0f, -1.0f,
+	-1.0f, -1.0f,  1.0f,
+	 1.0f, -1.0f,  1.0f
+};
+
+int main()
+{
+	//skybox
+	vector<string> faces =
+	{
+			"skybox/right4.jpg",
+			"skybox/left4.jpg",
+			"skybox/top4.jpg",
+			"skybox/bottom4.jpg",
+			"skybox/front4.jpg",
+			"skybox/back4.jpg"
+	};
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+	int width, height, nrChannels;
+	for (unsigned int i = 0; i < faces.size(); i++)
+	{
+		unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	unsigned int cubemapTexture = textureID;
+
+	///skybox
+	GLuint vbo, vao;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+
+
+	//bind VAO
+	glBindVertexArray(vao);
+
+	//bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
+
+	//set attribute pointers
+	glVertexAttribPointer(
+		0,                  // attribute 0, must match the layout in the shader.
+		3,                  // size of each attribute
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		3 * sizeof(float),  // stride
+		(void*)0            // array buffer offset
+	);
+	glEnableVertexAttribArray(0);
+
+	glClearColor(0.2f, 0.8f, 1.0f, 1.0f);
+
+	
+
+	//building and compiling shader program
+	Shader shader("Shaders/vertex_shader.glsl", "Shaders/fragment_shader.glsl");
+	Shader sunShader("Shaders/sun_vertex_shader.glsl", "Shaders/sun_fragment_shader.glsl");
+	Shader terrainShader("Shaders/terrain_vertex_shader.glsl", "Shaders/terrain_fragment_shader.glsl");
+	Shader skyboxShader("Shaders/skybox_vertex_shader.glsl", "Shaders/skybox_fragment_shader.glsl");
+	//Textures
+	GLuint tex = loadBMP("Resources/Textures/wood.bmp");
+	GLuint tex2 = loadBMP("Resources/Textures/rock.bmp");
+	GLuint tex3 = loadBMP("Resources/Textures/grasss.bmp");
+	GLuint heartTex = loadBMP("Resources/Textures/redtext.bmp");
+	GLuint manTex = loadBMP("Resources/Textures/bltext.bmp");
+	//GLuint ringTex = loadBMP("Resources/Textures/rock.bmp");
+	GLuint pineTex = loadBMP("Resources/Textures/leaf1.bmp");
+	GLuint barkTex = loadBMP("Resources/Textures/bark1.bmp");
+
+	
+
+	glEnable(GL_DEPTH_TEST);
+
+	//Test custom mesh loading
+	std::vector<Vertex> vert;
+	vert.push_back(Vertex());
+	vert[0].pos = glm::vec3(10.5f, 10.5f, 0.0f);
+	vert[0].textureCoords = glm::vec2(1.0f, 1.0f);
+
+	vert.push_back(Vertex());
+	vert[1].pos = glm::vec3(10.5f, -10.5f, 0.0f);
+	vert[1].textureCoords = glm::vec2(1.0f, 0.0f);
+
+	vert.push_back(Vertex());
+	vert[2].pos = glm::vec3(-10.5f, -10.5f, 0.0f);
+	vert[2].textureCoords = glm::vec2(0.0f, 0.0f);
+
+	vert.push_back(Vertex());
+	vert[3].pos = glm::vec3(-10.5f, 10.5f, 0.0f);
+	vert[3].textureCoords = glm::vec2(0.0f, 1.0f);
+
+	vert[0].normals = glm::normalize(glm::cross(vert[1].pos - vert[0].pos, vert[3].pos - vert[0].pos));
+	vert[1].normals = glm::normalize(glm::cross(vert[2].pos - vert[1].pos, vert[0].pos - vert[1].pos));
+	vert[2].normals = glm::normalize(glm::cross(vert[3].pos - vert[2].pos, vert[1].pos - vert[2].pos));
+	vert[3].normals = glm::normalize(glm::cross(vert[0].pos - vert[3].pos, vert[2].pos - vert[3].pos));
+
+	std::vector<int> ind = { 0, 1, 3,   
+		1, 2, 3 };
+
+	std::vector<Texture> textures;
+	textures.push_back(Texture());
+	textures[0].id = tex;
+	textures[0].type = "texture_diffuse";
+
+	std::vector<Texture> textures2;
+	textures2.push_back(Texture());
+	textures2[0].id = tex2;
+	textures2[0].type = "texture_diffuse";
+
+	std::vector<Texture> textures3;
+	textures3.push_back(Texture());
+	textures3[0].id = tex3;
+	textures3[0].type = "texture_diffuse";
+
+
+	//heart texture
+	std::vector<Texture> heartTexture;
+	heartTexture.push_back(Texture());
+	heartTexture[0].id = heartTex;
+	heartTexture[0].type = "texture_diffuse";
+
+	//man texture
+	std::vector<Texture> manTexture;
+	manTexture.push_back(Texture());
+	manTexture[0].id = manTex;
+	manTexture[0].type = "texture_diffuse";
+
+	//std::vector<Texture> ringTexture;
+	//ringTexture.push_back(Texture());
+	//ringTexture[0].id = ringTex;
+	//ringTexture[0].type = "texture_diffuse";
+
+	//pine texture
+	std::vector<Texture> pineTexture;
+	pineTexture.push_back(Texture());
+	pineTexture[0].id = pineTex;
+	pineTexture[0].type = "texture_diffuse";
+
+   //pine texture 
+	std::vector<Texture> barkTexture;
+	barkTexture.push_back(Texture());
+	barkTexture[0].id = barkTex;
+	barkTexture[0].type = "texture_diffuse";
+
+	Mesh mesh(vert, ind, textures3);
+
+	// Create Obj files - easier :)
+	// we can add here our textures :)
+	MeshLoaderObj loader;
+	Mesh sun = loader.loadObj("Resources/Models/sphere.obj");
+	Mesh box = loader.loadObj("Resources/Models/cube.obj", textures);
+	Mesh plane = loader.loadObj("Resources/Models/plane1.obj", textures3);
+	Mesh heart = loader.loadObj("Resources/Models/heart.obj", heartTexture);
+	//Mesh man = loader.loadObj("Resources/Models/boy.obj", manTexture);
+	//Mesh ring = loader.loadObj("Resources/Models/Thering.obj", ringTexture);
+	Mesh pine = loader.loadObj("Resources/Models/tree1.obj", pineTexture);
+	Mesh man = loader.loadObj("Resources/Models/man.obj", manTexture);
+	Mesh bark = loader.loadObj("Resources/Models/bark1.obj", barkTexture);
+
+	//check if we close the window or press the escape button
+	while (!window.isPressed(GLFW_KEY_ESCAPE) &&
+		glfwWindowShouldClose(window.getWindow()) == 0)
+	{
+		window.clear();
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processKeyboardInput();
+
+		//test mouse input
+		if (window.isMousePressed(GLFW_MOUSE_BUTTON_LEFT))
+		{
+			std::cout << "Pressing mouse button" << std::endl;
+		}
+		 //// Code for the light ////
+
+		sunShader.use();
+
+		glm::mat4 ProjectionMatrix = glm::perspective(90.0f, window.getWidth() * 1.0f / window.getHeight(), 0.1f, 10000.0f);
+		glm::mat4 ViewMatrix = glm::lookAt(camera.getCameraPosition(), camera.getCameraPosition() + camera.getCameraViewDirection(), camera.getCameraUp());
+
+		GLuint MatrixID = glGetUniformLocation(sunShader.getId(), "MVP");
+
+		//Test for one Obj loading = light source
+
+		glm::mat4 ModelMatrix = glm::mat4(1.0);
+		ModelMatrix = glm::translate(ModelMatrix, lightPos);
+		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		sun.draw(sunShader);
+
+		//skybox
+		glDepthMask(GL_FALSE);
+		skyboxShader.use();
+		glm::mat4 projection = glm::mat4(1.0f);
+	
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::mat4(glm::mat3(camera.getViewMatrix()));
+		glUniformMatrix4fv(glGetUniformLocation(skyboxShader.getId(), "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(skyboxShader.getId(), "projection"), 1, GL_FALSE, glm::value_ptr(ProjectionMatrix));
+		glBindVertexArray(vao);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glDepthMask(GL_TRUE);
+
+		//// End code for the light ////
+
+		shader.use();
+
+		///// Test Obj files for box ////
+
+		GLuint MatrixID2 = glGetUniformLocation(shader.getId(), "MVP");
+		GLuint ModelMatrixID = glGetUniformLocation(shader.getId(), "model");
+
+
+		ModelMatrix = glm::mat4(1.0);
+		ModelMatrix = glm::rotate(ModelMatrix, -90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+		ModelMatrix = glm::scale(glm::vec3(0.3f));
+		ModelMatrix = glm::translate(ModelMatrix, glm::vec3(0.0f, 0.0f, 0.0f));
+		MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+		glUniformMatrix4fv(MatrixID2, 1, GL_FALSE, &MVP[0][0]);
+		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+		glUniform3f(glGetUniformLocation(shader.getId(), "lightColor"), lightColor.x, lightColor.y, lightColor.z);
+		glUniform3f(glGetUniformLocation(shader.getId(), "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+		glUniform3f(glGetUniformLocation(shader.getId(), "viewPos"), camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
+
+		box.draw(shader);
+
+
+		
+		
+		//heart position
+			ModelMatrix = glm::mat4(1.0);
+			ModelMatrix = glm::rotate(ModelMatrix, -90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+			ModelMatrix = glm::scale(glm::vec3(0.3f));
+			ModelMatrix = glm::translate(ModelMatrix, glm::vec3(10.0f, 50.0f, 0.0f));
+			MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+			glUniformMatrix4fv(MatrixID2, 1, GL_FALSE, &MVP[0][0]);
+			glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+
+			heart.draw(shader);
+
+		//man position
+			ModelMatrix = glm::mat4(1.0);
+			ModelMatrix = glm::rotate(ModelMatrix, -180.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+			ModelMatrix = glm::scale(glm::vec3(8.0f));
+			ModelMatrix = glm::translate(ModelMatrix, glm::vec3(-30.0f, -2.0f, -20.0f));
+			MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+			glUniformMatrix4fv(MatrixID2, 1, GL_FALSE, &MVP[0][0]);
+			glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+
+			man.draw(shader);
+
+		//pine(tree) position
+			
+				ModelMatrix = glm::mat4(1.0);
+				ModelMatrix = glm::rotate(ModelMatrix, -90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+				ModelMatrix = glm::scale(glm::vec3(5.0f));
+				ModelMatrix = glm::translate(ModelMatrix, glm::vec3(50.0f, -1.0f, 0.0f));
+				MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+				glUniformMatrix4fv(MatrixID2, 1, GL_FALSE, &MVP[0][0]);
+				glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+
+				pine.draw(shader);
+			
+
+		//bark(tree) postiton
+			ModelMatrix = glm::mat4(1.0);
+			ModelMatrix = glm::rotate(ModelMatrix, -90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+			//ModelMatrix = glm::scale(glm::vec3(2.0f));
+			ModelMatrix = glm::translate(ModelMatrix, glm::vec3(240.0f, -20.0f, -3.0f));
+			MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+			glUniformMatrix4fv(MatrixID2, 1, GL_FALSE, &MVP[0][0]);
+			glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+
+			bark.draw(shader);
+
+		///// Test plane Obj file //////
+	
+		terrainShader.use();
+		ModelMatrix = glm::mat4(1.0);
+		ModelMatrix = glm::translate(ModelMatrix, glm::vec3(0.0f, -20.0f, 0.0f));
+		MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+		glUniformMatrix4fv(MatrixID2, 1, GL_FALSE, &MVP[0][0]);
+		glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+		glUniform3f(glGetUniformLocation(shader.getId(), "lightColor"), lightColor.x, lightColor.y, lightColor.z);
+		glUniform3f(glGetUniformLocation(shader.getId(), "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+		glUniform3f(glGetUniformLocation(shader.getId(), "viewPos"), camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
+
+		plane.draw(terrainShader);
+
+		window.update();
+	}
+	// Cleanup
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &vao);
+
+}
+
+void processKeyboardInput()
+{
+	float cameraSpeed = 30 * deltaTime;
+
+	//translation
+	if (window.isPressed(GLFW_KEY_W))
+		camera.keyboardMoveFront(cameraSpeed);
+	if (window.isPressed(GLFW_KEY_S))
+		camera.keyboardMoveBack(cameraSpeed);
+	if (window.isPressed(GLFW_KEY_A))
+		camera.keyboardMoveLeft(cameraSpeed);
+	if (window.isPressed(GLFW_KEY_D))
+		camera.keyboardMoveRight(cameraSpeed);
+	if (window.isPressed(GLFW_KEY_R))
+		camera.keyboardMoveUp(cameraSpeed);
+	if (window.isPressed(GLFW_KEY_F))
+		camera.keyboardMoveDown(cameraSpeed);
+
+	//rotation
+	if (window.isPressed(GLFW_KEY_LEFT))
+		camera.rotateOy(cameraSpeed);
+	if (window.isPressed(GLFW_KEY_RIGHT))
+		camera.rotateOy(-cameraSpeed);
+	if (window.isPressed(GLFW_KEY_UP))
+		camera.rotateOx(cameraSpeed);
+	if (window.isPressed(GLFW_KEY_DOWN))
+		camera.rotateOx(-cameraSpeed);
+}
+
